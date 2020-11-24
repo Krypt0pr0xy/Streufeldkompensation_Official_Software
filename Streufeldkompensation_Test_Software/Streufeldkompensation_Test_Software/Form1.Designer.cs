193eb71a0df335f71b590d31bbb634c7648c1293
krypto <krypt0pr0xy@mail.ch>
2020-11-24 11:12:28 +0100

    Update

--- Streufeldkompensation_Test_Software/Streufeldkompensation_Test_Software/Form1.Designer.cs
+++ Streufeldkompensation_Test_Software/Streufeldkompensation_Test_Software/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cb_Ports = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_OpenPort = new System.Windows.Forms.Button();
+            this.bt_ClosePort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cb_Ports
+            // 
+            this.cb_Ports.FormattingEnabled = true;
+            this.cb_Ports.Location = new System.Drawing.Point(31, 40);
+            this.cb_Ports.Name = "cb_Ports";
+            this.cb_Ports.Size = new System.Drawing.Size(119, 21);
+            this.cb_Ports.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Port:";
+            // 
+            // bt_OpenPort
+            // 
+            this.bt_OpenPort.Location = new System.Drawing.Point(156, 38);
+            this.bt_OpenPort.Name = "bt_OpenPort";
+            this.bt_OpenPort.Size = new System.Drawing.Size(75, 23);
+            this.bt_OpenPort.TabIndex = 2;
+            this.bt_OpenPort.Text = "Open Port";
+            this.bt_OpenPort.UseVisualStyleBackColor = true;
+            this.bt_OpenPort.Click += new System.EventHandler(this.bt_OpenPort_Click);
+            // 
+            // bt_ClosePort
+            // 
+            this.bt_ClosePort.Location = new System.Drawing.Point(156, 38);
+            this.bt_ClosePort.Name = "bt_ClosePort";
+            this.bt_ClosePort.Size = new System.Drawing.Size(75, 23);
+            this.bt_ClosePort.TabIndex = 3;
+            this.bt_ClosePort.Text = "Close Port";
+            this.bt_ClosePort.UseVisualStyleBackColor = true;
+            this.bt_ClosePort.Visible = false;
+            this.bt_ClosePort.Click += new System.EventHandler(this.bt_ClosePort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(674, 291);
+            this.Controls.Add(this.bt_ClosePort);
+            this.Controls.Add(this.bt_OpenPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb_Ports);
+            this.Name = "Form1";
+            this.Text = "Streufeldkompensation Test Software";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cb_Ports;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bt_OpenPort;
+        private System.Windows.Forms.Button bt_ClosePort;
     }
 }
 
--- Streufeldkompensation_Test_Software/Streufeldkompensation_Test_Software/Form1.cs
+++ Streufeldkompensation_Test_Software/Streufeldkompensation_Test_Software/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management; // need to add System.Management to your project references.
+
 
 namespace Streufeldkompensation_Test_Software
 {
     public partial class Form1 : Form
     {
+        public System.IO.Ports.SerialPort sport;
+        bool connected = false;
+
         public Form1()
         {
             InitializeComponent();
+            foreach (String s in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                cb_Ports.Items.Add(s);
+            }
+        }
+
+        private void bt_OpenPort_Click(object sender, EventArgs e)
+        {
+            if (connected == false)
+            {
+                String port = cb_Ports.Text;
+                try
+                {
+                    sport = new System.IO.Ports.SerialPort(
+                    port, 9600, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+                    sport.Open();
+                    if (sport.IsOpen)
+                    {
+                        connected = true;
+                        bt_ClosePort.Visible = true;
+                        bt_OpenPort.Visible = false;
+                    }
+                }
+                catch (Exception ex) { MessageBox.Show("No Port is Selected"); }
+                
+                
+            }
+        }
+
+        private void bt_ClosePort_Click(object sender, EventArgs e)
+        {
+            if(sport.IsOpen)
+            {
+                sport.Close();
+                connected = false;
+                bt_ClosePort.Visible = false;
+                bt_OpenPort.Visible = true;
+            }
         }
     }
 }
